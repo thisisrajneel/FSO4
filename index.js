6a9cb0485870542b7cd4b9cc11eca5240c9bffcb
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-04-05 13:52:31 +0530

    4.1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -11,10 +12,22 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
+console.log('connecting to ', mongoUrl);
 mongoose.connect(mongoUrl)
+        .then(result => {
+            console.log('connected to database');
+        })
 
 app.use(cors())
 app.use(express.json())
@@ -37,7 +50,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

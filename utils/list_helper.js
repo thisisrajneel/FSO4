84c8aa9605dd2e8a9ca240f2265f0c31630d1a7e
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-04-11 00:08:29 +0530

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, blogs) => {
+        return sum + blogs.likes
+    }
+
+    return blogs.length == 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }

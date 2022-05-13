721df7efa933018a04049af41c9ebe09b8962a41
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-05-13 19:28:35 +0530

    4.8

---
+++ tests/blog_api.test.js
@@ -0,0 +1,41 @@
+const Blog = require('../models/blog')
+const mongoose = require('mongoose')
+const app = require('../app')
+
+const supertest = require('supertest')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: 'Promise methods in Javascript',
+        author: 'Raj Neel',
+        url: 'www.hashnode.com',
+        likes: 100
+    },
+    {
+        title: 'Testing React apps',
+        author: 'Anu Raj',
+        url: 'www.dev.to',
+        likes: 200
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test('correct ammount of blogposts are returned', async () => {
+    const blogs = await api.get('/api/blogs')
+    
+    expect(blogs.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
